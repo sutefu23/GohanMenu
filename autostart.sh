ce1e9d5a9ae00333b80030a685a1a6b4986445e7
sutefu23 <sutefu23@gmail.com>
2020-11-12 08:43:51 +0900

    自動起動スクリプト 見直し

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,25 @@
-sleep 10
+#!/bin/bash -
+cd ~/GohanMenu
+while :
+  do
+    if [ "ping -c 1 -W 1 github.com" ]; then
+      echo "コネクション確認"
+      git pull
+      break
+    else
+      sleep 1
+      echo "コネクション確認リトライ"
+    fi
+  done
 
-cd /home/pi/GohanMenu
-git pull
 
-sudo python main.py
+while :
+  do
+    if [ "`ps ax | grep python main.py`" ]; then
+      # "ソフト起動中"
+      sleep 1
+    else
+      echo "ソフト起動を確認できないため起動処理"
+      sudo python main.py
+    fi
+  done
